3054f9156640d95d83636ea246c0d52f2f632015
EkaterinaShkolnikk <75201487+EkaterinaShkolnikk@users.noreply.github.com>
2026-05-31 16:32:50 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,45 +3,49 @@
 #include <fstream>
 #include <cstdlib>
 #include <ctime>
+#include <cstdint>
 #include "train.h"
 
-int measureOps(int n, int mode) {
+int measureOps(int n, int mode, unsigned int* seed) {
     Train train;
     for (int i = 0; i < n; i++) {
         bool state;
         if      (mode == 0) state = false;
         else if (mode == 1) state = true;
-        else                state = (rand() % 2 == 0);
+        else                state = (rand_r(seed) % 2 == 0);
         train.addCar(state);
     }
     train.getLength();
     return train.getOpCount();
 }
 
-
 int main() {
-    srand(static_cast<unsigned>(time(nullptr)));
+    unsigned int seed = static_cast<unsigned int>(time(nullptr));
+
     {
         Train t;
         for (int i = 0; i < 60; i++) t.addCar(false);
         std::cout << "Длина поезда (ожидается 60): "
-        << t.getLength() << std::endl;
+            << t.getLength() << std::endl;
         std::cout << "Число шагов: "
-        << t.getOpCount() << std::endl;
+            << t.getOpCount() << std::endl;
     }
 
     std::ofstream csv("result/data.csv");
     csv << "n,ops_off,ops_on,ops_rand\n";
-    const int STEP = 5;
-    const int MAX_N = 200;
-    const int REPEATS = 7;
+ 
+    const int STEP    =  5;
+    const int MAX_N   = 200;
+    const int REPEATS =  7;
 
     for (int n = 2; n <= MAX_N; n += STEP) {
-        int ops_off = measureOps(n, 0);
-        int ops_on  = measureOps(n, 1);
-        int64_t int64_t sum = 0;
-        for (int r = 0; r < REPEATS; r++) sum += measureOps(n, 2);
+        int ops_off = measureOps(n, 0, &seed);
+        int ops_on  = measureOps(n, 1, &seed);
+
+        int64_t sum = 0;
+        for (int r = 0; r < REPEATS; r++) sum += measureOps(n, 2, &seed);
         int ops_rand = static_cast<int>(sum / REPEATS);
+
         csv << n << "," << ops_off << "," << ops_on << "," << ops_rand << "\n";
     }
 
